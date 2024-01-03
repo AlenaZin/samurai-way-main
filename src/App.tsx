a8d1a204f2ba1debf7b65f3d9415271d247f2a05
AlenaZinovieva <sas1493@mail.ru>
2024-01-03 13:41:27 +0300

    Added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://img.freepik.com/premium-vector/bird-logo-vector_95982-44.jpg" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://kartin.papik.pro/uploads/posts/2023-06/1687110851_kartin-papik-pro-p-kartinki-s-kolibri-i-tsvetami-krasivie-61.jpg" />
+        </div>
+        
+        <div>
+          ava + discription
+        </div>
+
+        <div>
+          My posps
+          <div>
+            New post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
